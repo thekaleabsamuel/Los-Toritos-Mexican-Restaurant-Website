6480210115919ce7ecccd1cd0f72d42cb7bb321b
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-04-25 11:57:04 -0600

    all assets imported

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "/Users/donjuan/Downloads/projects/Los Toritos Mexican Restauraunt/los/src/Assets/Logo.svg"
+import { BsCart2 } from "react-icons/bs";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { Box,Drawer,ListItem, ListItemButton, ListItemIcon, ListItemText  } from "@mui/material"
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import CommentRounded from "@mui/icons-material";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
+
+
+const Navbar = () => {
+
+    const [openMenu, setOpenMenu] = React.useState(false);  
+    const menuOptions = [
+
+    ]
+
+    return (
+        <div>Navbar</div>
+    )
+}
+
+
+export default Navbar;
